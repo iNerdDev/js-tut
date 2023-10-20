5677773fa64ca1b26734e5ed0538cc12f1998957
iNerdDev <abuwaquash117@gmail.com>
2023-10-20 20:02:09 +0530

    3rd push

---
+++ app.js
@@ -0,0 +1,13 @@
+let grade = 'A'
+switch (grade) {
+    case "A":
+        console.log("1st division")
+        break;
+    case "B":
+        console.log("2nd division")
+        break;
+    case "C":
+        console.log("3rd division")
+    default:
+        break;
+}
