4d875c84056924c35a80af8772213898a3ed559f
iNerdDev <abuwaquash117@gmail.com>
2023-10-20 19:59:24 +0530

    2nd push

---
+++ app2.js
@@ -0,0 +1,93 @@
+// Arithmetic operator (1)
+// it is used to perform arithmetic operations on the number
+// let a=5
+// let b=2
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+// b++
+// console.log(b)
+// b--
+// console.log(b)
+
+
+// Assignment operator (2)
+// it assign the value to the variable
+// let x=5;
+// console.log(x)
+// x+=5
+// console.log(x)
+// x-=2
+// console.log(x)
+// x*=2
+// console.log(x)
+// x/=2
+// console.log(x)
+// x**=2
+// console.log(x)
+
+// right shift (it will shift the bits of x(value) by 5 bits towards right)
+// let x=100
+// x>>=5
+// console.log(x)
+
+// left shift (it will shift the bits of x(value) by 5 bits towards left)
+// let x=100
+// x<<=5
+// console.log(x)
+
+// comparison operator (3) (it is used in logical statement to check equality or difference b/w variables or values)
+// let txt1=10
+// let txt2=10
+// let result=txt1 < txt2
+// let result = txt1 == txt2
+// let result = txt1 === txt2
+// let result= txt1 !== txt2
+// let result= txt1>txt2
+// let result=txt1<txt2
+// let result= txt1>=txt2
+// let result=txt1<=txt2
+// console.log(result)
+
+// String operator (4)
+// let a="a"
+// let b="b"
+// let z=a+b
+// console.log(z)
+
+
+// logical operator (5)
+// let a=5
+// let b=6
+// if(a>0 && b>0){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+// let a=5
+// let b=6
+// if(a>0 || b>9){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+
+// Type operator (6)
+// let a="hello"
+// console.log(typeof(a))
+// a=Number(a)
+// console.log(typeof(a))
+
+// Ternary operator(7) (it assign the value to variable based on condition)
+// let age=18
+// let votable=(age<18) ? "can't vote":"can vote"
+// console.log(votable)
+
+
+
+
